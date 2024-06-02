32c782be9e7476a656d6d8c7fa0c1d5f1ff99630
thawbaan <sobanjawaid26@gmail.com>
2024-06-02 17:32:43 +0530

    get object count and get object names by pattern corrected to retreive from DB

--- src/main/java/com/awssdkstarter/aws_sdk_test/repository/BucketDetailRepository.java
+++ src/main/java/com/awssdkstarter/aws_sdk_test/repository/BucketDetailRepository.java
@@ -4,6 +4,10 @@ import com.awssdkstarter.aws_sdk_test.model.BucketDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BucketDetailRepository extends JpaRepository<BucketDetail, String> {
+
+    List<BucketDetail> findByBucketName(String bucketName);
 }
--- src/main/java/com/awssdkstarter/aws_sdk_test/service/S3CustomService.java
+++ src/main/java/com/awssdkstarter/aws_sdk_test/service/S3CustomService.java
@@ -31,70 +31,21 @@ public class S3CustomService implements IS3CustomServie{
         String REGION = "ap-south-1";      // Region name
         System.out.println("Bucket name: " + bucket);
 
-        // Instantiates a client
-        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
-                .withRegion(REGION).build();
-
-        try {
-            if (s3client.doesBucketExistV2(bucket)) {
-                System.out.println("Listing objects ...");
-                // List objects in a Bucket
-                ObjectListing objectListing = s3client.listObjects(new ListObjectsRequest()
-                        .withBucketName(bucket));
-                for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
-                    System.out.println(" - " + objectSummary.getKey() + "  " +
-                            "(size = " + objectSummary.getSize() + ")");
-                    if(isRegexMatching(pattern, objectSummary.getKey()))
-                        list.add(objectSummary.getKey());
-                }
-                System.out.println("Listed");
-
-            } else {
-                System.out.println("Error: Bucket does not exist!!");
+        List<BucketDetail> res = bucketDetailRepository.findByBucketName(bucket);
+        for (BucketDetail bucketDetail : res) {
+                System.out.println(" - " + bucketDetail.getObjectName() + "  " +
+                        "(size = " + bucketDetail.getSize() + ")");
+                if(isRegexMatching(pattern, bucketDetail.getObjectName()))
+                    list.add(bucketDetail.getObjectName());
             }
-        } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException, " +
-                    "which means your request made it " +
-                    "to Amazon S3, but was rejected with an error response " +
-                    "for some reason.");
-            System.out.println("Error Message:    " + ase.getMessage());
-            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-            System.out.println("Error Type:       " + ase.getErrorType());
-            System.out.println("Request ID:       " + ase.getRequestId());
-        } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException, " +
-                    "which means the client encountered " +
-                    "an internal error while trying to communicate" +
-                    " with S3, " +
-                    "such as not being able to access the network.");
-            System.out.println("Error Message: " + ace.getMessage());
-        }
-        s3client.shutdown();
-
         return list;
     }
 
     @Override
     public Integer getS3BucketObjectCount(String bucket) {
-        String REGION = "ap-south-1";      // Region name
-        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
-                .withRegion(REGION).build();
         int count = 0;
-        try {
-            if (s3client.doesBucketExistV2(bucket)) {
-                ObjectListing objectListing = s3client.listObjects(new ListObjectsRequest()
-                        .withBucketName(bucket));
-                count = objectListing.getObjectSummaries().size();
-            } else {
-                System.out.println("Error: Bucket does not exist!!");
-            }
-        } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException");
-        } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException");
-        }
-        s3client.shutdown();
+        List<BucketDetail> res = bucketDetailRepository.findByBucketName(bucket);
+        count = res.size();
         return count;
     }
 
